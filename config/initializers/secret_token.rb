7db32d759ed73de13416752c1b7ac3f36869e0eb
root <root>
2017-02-12 18:00:46 +0200

    1st comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBlog::Application.config.secret_token = '4f689d36d1c6f9cf34da99af38e59952d14a6861f44436f1d31b7baf73ea2321a545414b25f5fd8c27b81d6e4e0df55d692c3cff8d05b1dda4d47a0e3ac83703'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NewBlog::Application.config.session_store :cookie_store, key: '_new_blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NewBlog::Application.config.session_store :active_record_store
